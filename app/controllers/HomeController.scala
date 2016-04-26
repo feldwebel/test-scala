0252fbeb0e1bf0b726f7f220415aecc537643163
Dan Tereschenko <d.tereschenko@paymantix.com>
2016-04-26 21:10:45 +0300

    some features

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -2,12 +2,10 @@ package controllers
 
 import javax.inject._
 
-import play.api._
 import play.api.mvc._
-import models.PeopleModel
-import models.DB
 import play.api.data._
 import play.api.data.Forms._
+import models.PeopleModel
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
@@ -16,44 +14,22 @@ import play.api.data.Forms._
 @Singleton
 class HomeController @Inject() extends Controller {
 
-  /**
-   * Create an Action to render an HTML page with a welcome message.
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
-  }
-
-  val InputForm: Form[PeopleModel] = Form (
+  val PeopleForm : Form[PeopleModel] = Form (
     mapping(
       "name" -> text,
       "salary" -> number,
       "department" -> text
-    )(InputForm.apply)(InputForm.unapply)
+    )(PeopleModel.apply)(PeopleModel.unapply)
   )
 
-  def panel = Action {
-    Ok(views.html.panel("panel"))
+  def index = Action {
+    Ok(views.html.index(PeopleModel.show, PeopleModel.total))
   }
 
   def add = Action { implicit request =>
-    val record = InputForm.bindFromRequest.get
-    DB.save(record)
-    Redirect(routes.HomeController.panel)
-  }
-
-  def showAll = Action {
-    Ok("Show All")
-  }
-
-  def showSalary = Action {
-    Ok("Show Salary")
-  }
-
-  def find = Action {
-    Ok("Find User")
+    val record = PeopleForm.bindFromRequest.get
+    PeopleModel.save(record)
+    Redirect(routes.HomeController.index)
   }
 
 }
--- app/models/PeopleModel.scala
+++ app/models/PeopleModel.scala
@@ -1,12 +1,41 @@
 package models
 
-import play.api.libs.json.Json
+import scala.collection.immutable.List
 
 /**
   * Created by user on 26.04.16.
   */
-case class PeopleModel(name: String, salary: Integer, department: String)
+case class PeopleModel(name: String, salary: Int, department: String)
 
 object PeopleModel {
-  implicit val People = Json.format[PeopleModel]
+  var records = List.empty[PeopleModel]
+
+  def save(record: PeopleModel): Unit = {
+    records = record :: records
+  }
+
+  def show(): List[PeopleModel] = {
+    records.reverse
+  }
+
+  def find(name: String): Option[PeopleModel] = {
+    records.find(r => r.name == name)
+  }
+
+  def total(): Int = records.map(_.salary).sum
+
+  def average() {}
+
+  def average(department: String) {}
+
+  //  def totalSum(department: String): Int =
+  //    records.filter(r => r.department == department).map(_.salary).sum
+  //
+  //  def averageSum(department: String): Float = {
+  //    val items = records.filter(r => r.department == department).map(_.salary)
+  //    items.sum / items.length
+  //  }
+  //
+  //  def findUser(name: String): String =
+  //    records.find(r => r.name == name).toString
 }
